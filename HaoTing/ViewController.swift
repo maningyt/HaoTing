8d7d3bedf5c6e585ea2d91776dc25b83269e30d4
Ning Ma <ning@nings-macbook-pro.local>
2018-03-02 22:09:29 -0800

    Initial Commit

---
+++ HaoTing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HaoTing
+//
+//  Created by Ning Ma on 2018-03-02.
+//  Copyright © 2018 NM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
